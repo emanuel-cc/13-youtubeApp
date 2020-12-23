b02a18dc5b94bf40a432ae44602b9a608e1dc265
Emanuel Antonio <Emanuel Antonio@DESKTOP-10ULSAH.(none)>
2020-12-22 19:45:34 -0600

    Aplicación de videos de youtube

---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { YoutubeResponse } from '../models/youtube.models';
+
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyDwX2IsNuWESi7njE7fqlX9K2o2AKb6i8Y';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
+
+  constructor(private http:HttpClient) {}
+
+  getVideos(){
+    const url = `${this.youtubeUrl}/playlistItems`;
+    const params = new HttpParams()
+      .set('part', 'snippet')
+      .set('maxResults', '10')
+      .set('playlistId', this.playlist)
+      .set('key', this.apiKey)
+      .set('pageToken', this.nextPageToken);
+
+    return this.http.get<YoutubeResponse>(url, {params: params})
+      .pipe(
+        map(resp=>{
+          this.nextPageToken = resp.nextPageToken;
+          return resp.items;
+        }),
+        map(items=>{
+          return items.map(video=>video.snippet);
+        })
+      )
+  }
+}
